f9e5fcacc5c05bc42e4ac9a7aa4ff5b2d757e35a
Joseph Pickens <josephpickens@brandeis.edu>
2020-12-11 04:21:09 +0000

    implements all_vs_some pairing method

--- src/sumo_trainer.py
+++ src/sumo_trainer.py
@@ -51,7 +51,7 @@ class SumoTrainer:
         self.save_results(self.best_genomes, num_gen)
 
     def fitness_function(self, genomes, config):
-        genome_pairs = self.pair_species_all_vs_all(genomes)
+        genome_pairs = self.pair_species_all_vs_some(genomes, 5)
         genome_dict = defaultdict(int)
         for genome_pair in genome_pairs:
             latest_fitnesses = self.eval_genome_pair(genome_pair, config)
@@ -100,6 +100,20 @@ class SumoTrainer:
         return izip_longest(genomes[0:(len(genomes) // 2)], genomes[(len(genomes) // 2):],
                             fillvalue=genomes[0])
 
+    def pair_all_vs_some(self, genomes, n):
+        random.shuffle(genomes)
+        genome_pairs = []
+        i = 0
+        j = n
+        while j <= len(genomes):
+            genome_pairs.extend(self.pair_all_vs_all(genomes[i:j]))
+            i += n
+            j += n
+        if i < len(genomes):
+            genome_pairs.extend(self.pair_all_vs_all(genomes[i:]))
+        return genome_pairs
+
+
     def pair_species_all_vs_all(self, genomes):
         genomes_grouped = self.group_genomes_by_species(genomes)
         genome_pairs = []
@@ -112,6 +126,13 @@ class SumoTrainer:
         genome_pairs = []
         for gs in genomes_grouped:
             genome_pairs.extend(self.pair_one_vs_one(gs))
+        return genome_pairs
+
+    def pair_species_all_vs_some(self, genomes, n):
+        genomes_grouped = self.group_genomes_by_species(genomes)
+        genome_pairs = []
+        for gs in genomes_grouped:
+            genome_pairs.extend(self.pair_all_vs_some(gs, n))
         return genome_pairs
 
     def group_genomes_by_species(self, genomes):
